fb68f30fab2549f7c181b4820630fe4a08670fac
Wolary <Ktrnjh19>
2021-04-06 17:27:50 +0300

    Adding Files

--- src/test/java/tests/TestForm.java
+++ src/test/java/tests/TestForm.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
+
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
 
