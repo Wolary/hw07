be1ae4a0dd2ad34d5b05d259de1d13d7fc0ccdc8
Wolary <Ktrnjh19>
2021-04-06 17:59:48 +0300

    Adding Files

--- src/test/java/tests/TestForm.java
+++ src/test/java/tests/TestForm.java
@@ -10,7 +10,7 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
 
-public class TestForm {
+public class TestForm extends TestBase {
 
     @Test
     void FillForm() {
